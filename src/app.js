ce2622ba7d73a0df390819b81bac1cfcb788c1af
Przemysław Żabkiewicz <yevoonn@gmail.com>
2026-06-15 20:41:36 +0200

    Added /health endpoint to keep WS running

--- src/app.js
+++ src/app.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/health", (req, res) => {
+  res.status(200).json({ ok: true });
+});
+
 app.use("/api", routes);
 
 export default app;

216b9aa20d8f7c8379714d622c076e37edf2a662
Przemysław Żabkiewicz <yevoonn@gmail.com>
2026-07-05 19:40:51 +0200

    Configure CORS for production environment

--- src/app.js
+++ src/app.js
@@ -4,7 +4,19 @@ import routes from "./routes/index.js";
 
 const app = express();
 
-app.use(cors());
+const isProduction = process.env.NODE_ENV === "production";
+
+if (isProduction && !process.env.FRONTEND_URL) {
+  throw new Error("FRONTEND_URL environment variable is not defined.");
+}
+
+const corsOptions = isProduction
+  ? {
+      origin: process.env.FRONTEND_URL,
+    }
+  : {};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get("/health", (req, res) => {
